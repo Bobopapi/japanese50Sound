a2aac88ae7a4281e6799cd3a89df59192b21a4c9
Qu Haotian <bobopapi130@gmail.com>
2022-12-08 23:47:53 +0800

    practise on my weak sounds

--- japanese.py
+++ japanese.py
@@ -4,11 +4,20 @@ import random
 class Japanese:
     def __init__(self):
         self.roman = ('a','i','u','e','o','ka','ki','ku','ke','ko','sa','shi','su','se','so','ta','chi','tsu','te','to',
-        'na','ni','nu','ne','no','ha','hi','hu','he','ho','ma','mi','mu','me','mo','ya','yu','yo','ra','ri','ru','re','ro','wa','wo')
+        'na','ni','nu','ne','no','ha','hi','hu','he','ho','ma','mi','mu','me','mo','ya','yu','yo','ra','ri','ru','re','ro','wa','wo',
+        'n','ga','gi','gu','ge','go','za','ji','zu','ze','zo','da','di','du','de','do','ba','bi','bu','be','bo','pa','pi','pu','pe','po','ja','ju','jo')
         self.hiragana = ('あ','い','う','え','お','か','き','く','け','こ','さ','し','す','せ','そ','た','ち','つ','て','と',
-        'な','に','ぬ','ね','の','は','ひ','ふ','へ','ほ','ま','み','む','め','も','や','ゆ','よ','ら','り','る','れ','ろ','わ','を')
+        'な','に','ぬ','ね','の','は','ひ','ふ','へ','ほ','ま','み','む','め','も','や','ゆ','よ','ら','り','る','れ','ろ','わ','を','ん',
+        'が','ぎ','ぐ','げ','ご','ざ','じ','ず','ぜ','ぞ','だ','ぢ','づ','で','ど','ば','び','ぶ','べ','ぼ','ぱ','ぴ','ぷ','ぺ','ぽ','じゃ','じゅ','じょ')
         self.katakana = ('ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ','タ','チ','ツ','テ','ト',
-        'ナ','ニ','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ','マ','ミ','ム','メ','モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ')
+        'ナ','ニ','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ','マ','ミ','ム','メ','モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ','ン',
+        'ガ','ギ','グ','ゲ','ゴ','ザ','ジ','ズ','ゼ','ゾ','ダ','ヂ','ヅ','デ','ド','バ','ビ','ブ','ベ','ボ','パ','ピ','プ','ペ','ポ','ジャ','ジュ','ジョ')
+        self.unfamiliar = [6,8,9,10,11,12,14,16,17,18,19,20,23,25,27,29,30,32,33,38,40,41,42,43,44]
+        dakuon = [num for num in range(46,68)]
+        self.unfamiliar = self.unfamiliar + dakuon
+        self.roman = ( self.roman[num] for num in self.unfamiliar )
+        self.hiragana = ( self.hiragana[num] for num in self.unfamiliar )
+        self.katakana = ( self.katakana[num] for num in self.unfamiliar )
         self.dictionary = {r:(h,k) for r,h,k in zip(self.roman,self.hiragana,self.katakana)}
         self.counterReset(2)
         self.test()
